2aa8e04faabe8788b6d9deea93c9fdc9dde2d7a5
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-08-02 19:55:13 +0530

    feat: registered blueprints

--- vlab/__init__.py
+++ vlab/__init__.py
@@ -3,6 +3,9 @@
 from flask import Flask
 from vlab.settings import config
 
+from vlab import core
+from .views import blueprint as base_blueprint
+
 def create_app(config_object=config):
     """Create an application factory
 
@@ -15,4 +18,12 @@ def create_app(config_object=config):
     app.logger.info(f"Using {config_object.__class__.__name__}")
     app.logger.info(f"Debug mode is {config_object.DEBUG}")
 
+    register_blueprints(app)
+
     return app
+
+def register_blueprints(app: Flask):
+    """Registering blueprints."""
+
+    app.register_blueprint(base_blueprint)
+    app.register_blueprint(core.views.blueprint)
